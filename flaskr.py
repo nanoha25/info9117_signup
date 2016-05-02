c274b2fb12e8a213c1ad41aec15a41e5a15d871f
nanoha25 <nanohachina@takamachi.onmicrosoft.com>
2016-05-02 17:11:12 +1000

    Sync

--- flaskr.py
+++ flaskr.py
@@ -2,6 +2,9 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
+#more imports
+from contextlib import closing
+
 #configuration
 DATABASE = '/tmp/flaskr.db'
 DEBUG = True
@@ -16,6 +19,12 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode = 'r') as f: #"f" stands for what?
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route('/')
 def hello_world():
---
+++ schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE if EXISTS entries;
+CREATE TABLE entries
+(
+  id INTEGER PRIMARY KEY autoincrement,
+  title text NOT NULL,
+  text text NOT NULL
+);
