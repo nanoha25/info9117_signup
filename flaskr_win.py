1a12d65ffa3e478019cc3eeeac56f508b60303ac
nanoha25 <nanohachina@takamachi.onmicrosoft.com>
2016-05-05 15:20:31 +1000

    Code from flask tutorial.

--- flaskr_win.py
+++ flaskr_win.py
@@ -6,7 +6,8 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from contextlib import closing
 
 #configuration
-DATABASE = '/tmp/flaskr.db' #this is a path useable only on *nix system. Using this path under Windows will cause error.
+DATABASE = '/tmp/flaskr.db' # this is a path useable only on *nix system. Using this path under Windows will cause error.
+DATABASE_WIN = "flaskr.db" # just use relative path and let python to handle everythine else. So problem avoided.
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -20,10 +21,19 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def connect_db_win():
+    return sqlite3.connect(app.config['DATABASE_WIN'])
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode = 'r') as f: #"f" stands for the name of an object created through app.open_resource
             db.cursor().executescript(f.read()) #f.read() stands for an action which reads info from object with the name "f"
+        db.commit()
+
+def init_db():
+    with closing(connect_db_win()) as db:
+        with app.open_resource('schema.sql', mode = 'r') as f:
+            db.cursor().executescript(f.read())
         db.commit()
 
 #tutorial step 4
@@ -38,42 +48,14 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
-#tutorial step 5
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-#
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
